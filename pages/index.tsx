0250ff535bf252172e207d16e180ab99cee5c544
srsHitGub <67808459+srsHitGub@users.noreply.github.com>
2022-04-01 11:13:27 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,13 @@ import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Products from '../components/Products'
-import MusicPlayer from '../components/MusicPlayer'
 import Minter from '../components/Minter'
 
 const Home = () => {
   return (
     <>
       <Head>
-        <title>Mintbase Engineering</title>
+        <title>Blob NFT Minter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
@@ -20,7 +19,6 @@ const Home = () => {
       <Hero />
       {/* <Products storeId="wildeverse.mintbase1.near" /> */}
       {/* <Products storeId="hellovirtualworld.mintbase1.near" /> */}
-      <MusicPlayer />
       <Minter />
       <Footer />
     </>
